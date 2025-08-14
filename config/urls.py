0690a418b87fe39a01b518368463e0df90b24985
chanel241 <chanelbirimbi@gmail.com>
2025-08-14 12:00:21 +0100

    Confirmer la configuration serve pour les médias

--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,10 @@
+# config/urls.py
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -16,7 +18,8 @@ urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
 ]
 
-# Servir les médias en développement, S3 gère en production
+if not settings.DEBUG:
+    urlpatterns += [path('media/<path:path>', serve, {'document_root': settings.MEDIA_ROOT})]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
